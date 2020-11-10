49481c055da65cd0e1f10643d94ea662b49826bb
SuicideDog <denarik159>
2020-11-11 00:32:37 +0200

    time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <section>
+    <div className="time hoursmin">
+      <h2 id="hours">00</h2>
+      <h2 id="colon">:</h2>
+      <h2 id="minutes">00</h2>
     </div>
+    <div className="time sec">
+      <span id="ampm"></span>
+      <h2 id="seconds">00</h2>
+    </div>
+  </section>
   );
 }
+
+function Real_time()
+{
+    let hours = document.getElementById('hours');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+    let ampm = document.getElementById('ampm');
+
+    let real_hours = new Date().getHours();
+    let real_minutes = new Date().getMinutes();
+    let real_seconds = new Date().getSeconds();
+    let real_ampm;
+
+    if(real_hours <= 12){
+      real_ampm = 'AM';
+    }
+    else{
+      real_ampm = 'PM';
+    }
+
+    hours.innerHTML = real_hours;
+    minutes.innerHTML = real_minutes;
+    seconds.innerHTML = real_seconds;
+    ampm.innerHTML = real_ampm;
+
+}
+
+setInterval(Real_time, 1000);
 
 export default App;
